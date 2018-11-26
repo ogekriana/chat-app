eb43b068f0d680e9511bd244ce5c0dfa2b6a07c3
ogekriana <ogek.riana@gmail.com>
2018-11-26 09:41:41 +0800

    add newMessage and createMessage event

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,18 @@
 var socket = io();
+
 socket.on('connect', function() {
 	console.log("connected to server")
+
+	socket.emit("createMessage", {
+		from: 'ogekrianamd',
+		text: 'hello email from server'
+	})
 })
-socket.emit("createEmail", {
-	to: 'rianamahliadewi@gmail.com',
-	text: 'hello email from server'
-})
+
 socket.on('disconnect', function() {
 	console.log("disconnected from server")
 })
-socket.on('newEmail', function(email) {
-	console.log("new email", email)
+
+socket.on('newMessage', function(newMessage) {
+	console.log("newMessage", newMessage)
 })
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const http = require('http')
 const socketIO = require('socket.io')
 
-const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
 const app = express()
 var server = http.createServer(app)
 var io = socketIO(server)
@@ -14,11 +14,16 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
 	console.log(`new user connected`)
 
-	socket.emit("newEmail", {
+	socket.emit("newMessage", {
 		from: 'ogek.riana@gmail.com',
 		content: 'hello what is going on?',
 		createdAt: 123
 	})
+
+	socket.on('createMessage', (message) => {
+		console.log("message", message)
+	})
+
 	socket.on('disconnect', () => {
 		console.log(`user was disconnected`)		
 	})
